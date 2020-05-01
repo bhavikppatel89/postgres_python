2e69db2a0f0ad1796c6bab7b2ae1b99f9b859f29
Bhavik Patel <bhavikppatel89@gmail.com>
2020-05-01 20:20:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+# @app.route("/my_example")
+# def hello():
+#     return "Hello Motherfucker!"
+#
+# if __name__ == "__main__":
+#     app.run()
+
+from app import config
+
+
+config()
